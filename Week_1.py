dbfa6d5440091285790b2e7504202227851467e6
BarsukovSergey <BraveBars@gmail.com>
2022-05-19 23:56:24 +0300

    hw_2

--- Week_1.py
+++ Week_1.py
@@ -1,2 +1,10 @@
 # 100A
-print("A" * 100)
+#print("A" * 100)
+
+# Пингвины.
+N = int(input())
+print('   _~_    ' * N)
+print('  (o o)   ' * N)
+print(' /  V  \  ' * N)
+print('/(  _  )\ ' * N)
+print('  ^^ ^^   ' * N)
