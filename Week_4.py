07eb22766731ae769a20e47edc79b4c076afc193
BarsukovSergey <BraveBars@gmail.com>
2022-07-04 01:54:23 +0300

    hw_2

--- Week_4.py
+++ Week_4.py
@@ -9,3 +9,14 @@ def min4(a, b, c, d):
 
 a, b, c, d = input(), input(), input(), input()
 print(min4(a, b, c, d))
+
+# 2. Длина отрезка. Даны четыре действительных числа: x₁, y₁, x₂, y₂. Напишите функцию
+# distance(x1, y1, x2, y2), вычисляющую расстояние между точками (x₁,y₁) и (x₂,y₂).
+# Считайте четыре действительных числа и выведите результат работы этой функции.
+def distance(x1, y1, x2, y2):
+    d = ((x2 - x1) ** 2 + (y2 - y1) ** 2)**0.5
+    return d
+
+
+x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
+print(distance(x1, y1, x2, y2))
