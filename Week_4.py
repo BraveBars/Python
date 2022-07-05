f828ade17366e1cf166eb9a0e18d4250b9176933
BarsukovSergey <BraveBars@gmail.com>
2022-07-05 23:31:14 +0300

    hw_6

--- Week_4.py
+++ Week_4.py
@@ -73,3 +73,22 @@ if IsPointInSquare(x, y):
     print('Yes')
 else:
     print('No')
+
+
+# 6. Принадлежит ли точка кругу. Даны пять действительных чисел: x, y, xc, yc, r.
+# Проверьте, принадлежит ли точка (x,y) кругу с центром (xc, yc) и радиусом r.
+# Если точка принадлежит кругу, выведите слово YES, иначе выведите слово NO.
+# Решение должно содержать функцию IsPointInCircle(x, y, xc, yc, r), возвращающую True,
+# если точка принадлежит кругу и False, если не принадлежит.
+# Основная программа должна считать координаты точки, вызвать функцию IsPointInCircle и в
+# зависимости от возвращенного значения вывести на экран необходимое сообщение. Функция
+# IsPointInCircle не должна содержать инструкцию if.
+def IsPointInCircle(x, y, xc, yc, r):
+    return ((x - xc)**2 + (y - yc)**2)**0.5 <= r
+
+x, y, xc, yc, r = float(input()), float(input()), \
+                  float(input()), float(input()), float(input())
+if IsPointInCircle(x, y, xc, yc, r):
+    print('Yes')
+else:
+    print('No')
