39ab04f7087aca786566d3868c104a5c47f29525
BarsukovSergey <BraveBars@gmail.com>
2022-07-05 23:29:33 +0300

    hw_4

--- Week_4.py
+++ Week_4.py
@@ -17,7 +17,6 @@ def distance(x1, y1, x2, y2):
     d = ((x2 - x1) ** 2 + (y2 - y1) ** 2)**0.5
     return d
 
-
 x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
 print(distance(x1, y1, x2, y2))
 
@@ -31,7 +30,30 @@ def perimeter(x1, y1, x2, y2, x3, y3):
     g = d + e + f
     return g
 
-
 x1, y1, x2, y2, x3, y3 = int(input()), int(input()), int(input()),\
                          int(input()), int(input()), int(input())
 print(perimeter(x1, y1, x2, y2, x3, y3))
+
+# 4. Принадлежит ли точка квадрату - 1. Даны два действительных числа x и y(оба от -1 до 1).
+# Проверьте, принадлежит ли точка с координатами (x,y) заштрихованному квадрату (включая его границу).
+# Если точка принадлежит квадрату, выведите слово YES, иначе выведите слово NO.
+# На рисунке сетка проведена с шагом 1.
+#
+# Решение должно содержать функцию IsPointInSquare(x, y), возвращающую True, если точка принадлежит
+# квадрату и False, если не принадлежит. Основная программа должна считать координаты точки, вызвать
+# функцию IsPointInSquare и в зависимости от возвращенного значения вывести на экран необходимое
+# сообщение. Функция IsPointInSquare не должна содержать инструкцию if.
+def IsPointInSquare(x, y):
+    return abs(x) <= 1 and abs(y) <= 1
+
+x, y = float(input()), float(input())
+if IsPointInSquare(x, y):
+    print('Yes')
+else:
+    print('No')
+# БОЛЕЕ ИНТЕРЕНСЫЙ ВАРИАНТ РЕШЕНИЯ ЗАДАЧИ
+def IsPointInSquare(x, y):
+    return x**2 <= 1 and y**2 <= 1
+
+a, b = float(input()), float(input())
+print(IsPointInSquare(a, b) * 'YES' + 0**IsPointInSquare(a, b) * 'NO')
