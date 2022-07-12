c85ce0958ef0ac4b1ad9f9b844b97b1d3ee45ec9
BarsukovSergey <BraveBars@gmail.com>
2022-07-12 23:50:20 +0300

    hw_16

--- Week_4.py
+++ Week_4.py
@@ -257,3 +257,27 @@ def seq():
 
 
 seq()
+
+
+# 16. Сократите дробь. Даны два натуральных числа n и m.
+# Сократите дробь (n / m), то есть выведите два других числа p и q таких, что (n / m) = (p / q)
+# и дробь (p / q) — несократимая.
+# Решение оформите в виде функции ReduceFraction(n, m), получающая значения n и m и возвращающей
+# кортеж из двух чисел: return p, q.
+# Тогда вывод можно будет оформить как print(*ReduceFraction(n, m)).
+def gcd(a, b):
+    if b == 0:
+        return a
+    elif a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+
+def asd(n, m):
+    c = gcd(n, m)
+    return (n // c, m // c)
+
+
+n = int(input())
+m = int(input())
+print(*asd(n, m))
